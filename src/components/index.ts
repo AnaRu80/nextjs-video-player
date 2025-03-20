a0cab6b7eb6a3e0032de0f652669164b88f1ddd0
Ana Ruiz <ruizana80@gmail.com>
2025-03-20 12:24:53 -0400

    comment videoplayer

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
-export { default as VideoPlayer } from './VideoPlayer';
+// export { default as VideoPlayer } from './VideoPlayer';
 export { default as VideoControls } from './VideoControlers';
 export { default as VideoInfo } from './VideoInfo';
 export { default as VideoHeader } from './VideoHeader';
